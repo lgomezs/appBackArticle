242812410a6a8d257e9d55360898afdf335238d6
Miguel Gomez <luismgsa@ext.inditex.com>
2023-07-03 19:35:46 -0500

    initial commit

--- src/test/java/pe/com/example/appBackArticle/AppBackArticleApplicationTests.java
+++ src/test/java/pe/com/example/appBackArticle/AppBackArticleApplicationTests.java
@@ -1,13 +1,8 @@
 package pe.com.example.appBackArticle;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AppBackArticleApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
