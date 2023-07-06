8059ee09ec759035ccd2aa20ca8ceb5dc9e14ce6
Miguel Gomez <luismgsa@ext.inditex.com>
2023-07-05 20:08:45 -0500

    initial commit

--- src/main/java/pe/com/example/appBackArticle/AppBackArticleApplication.java
+++ src/main/java/pe/com/example/appBackArticle/AppBackArticleApplication.java
@@ -2,14 +2,12 @@ package pe.com.example.appBackArticle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("pe.com.example")
 @SpringBootApplication
 public class AppBackArticleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppBackArticleApplication.class, args);
-	}
+  public static void main(final String[] args) {
+    SpringApplication.run(AppBackArticleApplication.class, args);
+  }
 
 }
